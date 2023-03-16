43252a63c02ed0576a21a8c9d834648de2586c0f
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2023-03-15 23:17:59 -0700

    Done

--- COMP003A.FINALPROJECTT/Program.cs
+++ COMP003A.FINALPROJECTT/Program.cs
@@ -27,80 +27,62 @@ namespace _003A.FInalProject
             Console.Write("Enter Last Name: ");
             lastName = Console.ReadLine();
 
+
             //userinput age
-            bool isValid = false;
-            int year;
-            do
-            {
-                Console.WriteLine("Enter Birth Year: ");
-
-
-                if (year <= 1900 || year >= 2030) ;
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-                else
-                {
-                    isValid = true;
-                }
-            }while(isValid);
+            int ageInput;
+            Console.WriteLine("Enter birth year: ");
+            ageInput = Convert.ToInt32(Console.ReadLine());
+            int ageOutput = Ageuser(ageInput);
+            
 
             ///userinput gender
             string genderUser;
-            Console.WriteLine("Please Enter Gender: (M, F, O)");
-            genderUser = Console.ReadLine();
-            char genderInput = Convert.ToChar(genderUser);
+                Console.WriteLine("Please Enter Gender: (M, F, O)");
+                genderUser = Console.ReadLine();
+                char genderInput = Convert.ToChar(genderUser);
 
-            var questions = new List<string>();
+                var questions = new List<string>();
 
-            questions.Add($"Question 1: Do you take meds");
+                questions.Add($"Question 1: Do you take meds");
 
-            questions.Add("Question 2: Do you play any sports");
+                questions.Add("Question 2: Do you play any sports");
 
-            questions.Add("Question 3: Have you had any serious injuries");
+                questions.Add("Question 3: Have you had any serious injuries");
 
-            questions.Add("Question 4: How tall are you");
+                questions.Add("Question 4: How tall are you");
 
-            questions.Add("Question 5: How much do you weigh");
+                questions.Add("Question 5: How much do you weigh");
 
-            questions.Add("Question 6: Do you drink a lot of water");
+                questions.Add("Question 6: Do you drink a lot of water");
 
-            questions.Add("Question 7: Have you smoked");
+                questions.Add("Question 7: Have you smoked");
 
-            questions.Add("Question 8: Have you drank alcohol");
+                questions.Add("Question 8: Have you drank alcohol");
 
-            questions.Add("Question 9: Have you ever passed out?");
+                questions.Add("Question 9: Have you ever passed out?");
 
-            questions.Add("Question 10: Has blood ever come out while you were peeing?");
+                questions.Add("Question 10: Has blood ever come out while you were peeing?");
 
-
-            var answers = new List<string>();
+                var responses = new List<string>();
 
             foreach (var question in questions)
             {
-                if (answers != null && answers.Any())
-                {
-                    Console.WriteLine("Please re-enter answer");
-                }
                 Console.WriteLine(question);
+                
+                    
+                
+                responses.Add(Console.ReadLine());
+            }
 
-                answers.Add(Console.ReadLine());
-
-                Console.Write("SUMMARY");
-                Console.Write("\n");//breaking the line
-                Username(lastName, firstName);//displaying the user input last name and first name
-                Console.Write(inputAge);
-                Console.Write("\n");
-                Genderinput(genderInput);
-
-
-
-                for (int i = 0; i < questions.Count; i++)
-                {
-                    Console.WriteLine(questions[i]);
-                    Console.WriteLine(answers[i]);
-                }
-
+            Console.Write("SUMMARY");
+            Console.Write("\n");//breaking the line
+            Username(lastName, firstName);//displaying the user input last name and first name
+            Console.WriteLine(ageOutput);
+            Genderinput(genderInput);
+            for (int i = 0; i < questions.Count; i++)
+            {
+                Console.WriteLine(questions[i]);
+                Console.WriteLine(responses[i]);
             }
 
             /// <summary>
@@ -109,46 +91,81 @@ namespace _003A.FInalProject
             /// <param name="lastName"></param>
             /// <param name="firstName"></param>
             static void Username(string lastName, string firstName)
-            {
-                Console.WriteLine($"Full Name:{lastName}, {firstName}");
-            }
+                {
+                    Console.WriteLine($"Full Name:{lastName}, {firstName}");
+                }
 
+                /// <summary>
+                /// Getting the users age by getting the users birth year and subtracting it with the current year (2023)
+                /// </summary>
+                /// <param name="year"></param>
+                /// <returns></returns>
+
+
+
+
+
+                /// <summary>
+                /// Getting the user input gender by using a if else loop based if the user enters M, F, or O
+                /// </summary>
+                /// <param name="gender"></param>
+                static void Genderinput(char gender)
+                {
+                    if (gender == ' ')
+                    {
+                        Console.WriteLine("Please re-enter age");
+                    }
+                    if (gender == 'M')
+                    {
+                        Console.WriteLine("Gender: Male");
+                    }
+                    else if (gender == 'F')
+                    {
+                        Console.WriteLine("Gender: Female");
+                    }
+                    else if (gender == 'O')
+                    {
+                        Console.WriteLine("Others not listed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid gender");
+                    }
+                }
+
+
+           
             /// <summary>
-            /// Getting the users age by getting the users birth year and subtracting it with the current year (2023)
+            /// Calculating the users age input and validating it to make sure they cant enter anything wrong
             /// </summary>
             /// <param name="year"></param>
-            /// <returns></returns>
-
-            
-
-            
-
-            /// <summary>
-            /// Getting the user input gender by using a if else loop based if the user enters M, F, or O
-            /// </summary>
-            /// <param name="gender"></param>
-            static void Genderinput(char gender)
+            static int Ageuser(int year)
             {
-                if (gender == ' ')
+                bool isValid = false;
+                do
                 {
-                    Console.WriteLine("Please re-enter age");
-                }
-                if (gender == 'M')
-                {
-                    Console.WriteLine("Gender: Male");
-                }
-                else if (gender == 'F')
-                {
-                    Console.WriteLine("Gender: Female");
-                }
-                else if (gender == 'O')
-                {
-                    Console.WriteLine("Others not listed");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid gender");
-                }
+                    Console.WriteLine("Enter Birth Year");
+
+                    try
+                    {
+                        year = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid Input!");
+                        continue;
+                    }
+
+                    if (year <= 1900 || year >= 2030)
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+                    else
+                    {
+                        isValid = true;
+                    }
+                } while(!isValid);
+                return 2023 - year;
             }
 
 
